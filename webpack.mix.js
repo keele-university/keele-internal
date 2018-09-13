ccd7453e878cc6e605d8ab2bc8e448b793080a7b
Chris Beard <coebe500@aol.com>
2018-09-13 17:50:44 +0100

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,36 @@
+const { mix } = require('laravel-mix');
+// const SvgStore = require('webpack-svgstore-plugin');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+// mix.js('resources/assets/js/app.js', 'public/js')
+//     .webpackConfig({
+//         plugins: [
+//             new SvgStore({
+//                 svgoOptions: {
+//                     plugins: [
+//                         { removeTitle: true }
+//                     ]
+//                 },
+//                 prefix: 'icon-'
+//             })
+//         ]
+//     })
+    mix.js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    // .sass('resources/assets/sass/pattern.scss', 'public/css')
+    // .copy('media', 'public/media')
+    .copy('mix-manifest.json', 'public/')
+    .browserSync({
+        // proxy: 'keele.dev'
+        proxy: 'https://keele-internal.dev'
+    });
