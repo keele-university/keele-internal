ae843aa3af4c46175575d89c237a6c1f751b50b4
Chris Beard <bearduk@users.noreply.github.com>
2018-09-14 10:49:15 +0100

    vue and base block tests

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,13 @@
 import 'bootstrap';
+window._ = require('lodash');
+window.Vue = require('vue');
+
+Vue.component('vuetest', require('./components/test-vue/Vue-test.vue'));
+
+// force devtools to load in prod
+// Vue.config.debug = true;
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el: '#page-wrapper'
+});
---
+++ resources/assets/js/components/test-vue/Vue-test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h1>Test Vue component</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
