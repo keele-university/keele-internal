ba2510bc89c510be1983bba5652197c903725749
Chris Beard <coebe500@aol.com>
2018-09-15 13:06:40 +0100

    jQuery base bootstraps running in app.js. Will work fine on rendered html from Vue components if classes and data attr etc are correct.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,6 +2,13 @@ import 'bootstrap';
 window._ = require('lodash');
 window.Vue = require('vue');
 
+// window.$ = require('jquery');
+var $ = require('jquery');
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
 Vue.component('vuetest', require('./components/test-vue/Vue-test.vue'));
 
 // force devtools to load in prod
--- resources/assets/js/components/test-vue/Vue-test.vue
+++ resources/assets/js/components/test-vue/Vue-test.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="container">
-        <div class="row">
-            <h1>Test Vue component</h1>
+        <div class="row justify-content-md-center py-4 mb-4">
+            <div class="col-10 text-center">
+                <h1>Test Vue component</h1>
+                <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Tooltip on top">Tooltip on top
+                </button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        
     }
 </script>
 
