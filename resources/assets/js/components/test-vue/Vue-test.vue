f8e58683666142fff878a3a7202016d93e906799
Chris Beard <bearduk@users.noreply.github.com>
2018-10-10 13:16:03 +0100

    Vue test component

--- resources/assets/js/components/test-vue/Vue-test.vue
+++ resources/assets/js/components/test-vue/Vue-test.vue
@@ -3,6 +3,7 @@
         <div class="row justify-content-md-center py-4 mb-4">
             <div class="col-10 text-center">
                 <h1>Test Vue component</h1>
+                <p>{{test}}</p>
                 <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Tooltip on top">Tooltip on top
                 </button>
             </div>
@@ -12,6 +13,12 @@
 
 <script>
     export default {
+        name: 'Vue test',
+        data(){
+            return {
+                test: 'Vue data 🍬'
+            }
+        }
     }
 </script>
 
