877fe6ae183d167bc7b29894ee7432a4d88bfa2a
Chris Beard <bearduk@users.noreply.github.com>
2018-10-15 13:31:07 +0100

    Fixed Jumbotron

--- t4/content-types/ki-jumbotron/ki-jumbotron.php
+++ t4/content-types/ki-jumbotron/ki-jumbotron.php
@@ -1,6 +1,6 @@
         <div class="jumbotron jumbotron-fluid text-center">
             <div class="container">
                 <t4 type="content" output="selective-output" modifiers="medialibrary, nav_sections" name="Heading" format="<h2 class=&quot;display-4&quot;>$value</h2>" />
-                <t4 type="content" output="selective-output" modifiers="medialibrary, nav_sections" name="Heading" format="<p class=&quot;lead&quot;>$value</p>" />
+                <t4 type="content" output="selective-output" modifiers="medialibrary, nav_sections" name="Lead text" format="<p class=&quot;lead&quot;>$value</p>" />
             </div>
         </div>
