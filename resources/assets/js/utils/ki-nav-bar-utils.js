96f949e9307dc197d9c6d363d85aedab5374bc0e
Chris Beard <coebe500@aol.com>
2018-10-18 17:33:28 +0100

    Transition only bgc

--- resources/assets/js/utils/ki-nav-bar-utils.js
+++ resources/assets/js/utils/ki-nav-bar-utils.js
@@ -4,24 +4,26 @@
 function init() {    
 
     // PRIMARY
-
     // find current branch from t4
     var kiNavBarPrimary = document.querySelector('.ki-nav-bar-primary .currentbranch0');
-    // find parent node (should be the surrounding li
-    var kiNavBarPrimaryParent = kiNavBarPrimary.parentNode;
-    
-    // now add the custom class to it
-    kiNavBarPrimaryParent.classList.add('js-currentbranch0-parent');
+    if (kiNavBarPrimary) {
+        // find parent node (should be the surrounding li
+        var kiNavBarPrimaryParent = kiNavBarPrimary.parentNode;
+        
+        // now add the custom class to it
+        kiNavBarPrimaryParent.classList.add('js-currentbranch0-parent');
+    }
 
     // SECONDARY
-
     // find current branch from t4
     var kiNavBarSecondary = document.querySelector('.ki-nav-bar-secondary .currentbranch0');
-    // find parent node (should be the surrounding li
-    var kiNavBarSecondaryParent = kiNavBarSecondary.parentNode;
-    
-    // now add the custom class to it
-    kiNavBarSecondaryParent.classList.add('js-currentbranch0-parent-secondary');
+    if (kiNavBarSecondary){
+        // find parent node (should be the surrounding li
+        var kiNavBarSecondaryParent = kiNavBarSecondary.parentNode;
+        
+        // now add the custom class to it
+        kiNavBarSecondaryParent.classList.add('js-currentbranch0-parent-secondary');
+    }
 }
 
 module.exports = {
