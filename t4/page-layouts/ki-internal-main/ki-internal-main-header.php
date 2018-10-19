9ed6b0130d89ff5146e1aa883d5a625e3d8fb884
Chris Beard <coebe500@aol.com>
2018-10-19 18:24:02 +0100

    COMPLETED base mobile navigation. Should be good to go now with a little tweaking.

--- t4/page-layouts/ki-internal-main/ki-internal-main-header.php
+++ t4/page-layouts/ki-internal-main/ki-internal-main-header.php
@@ -67,41 +67,8 @@
         
     <div class="ki-main-navs">
         <!-- navigation object : ki-level-5-nav --><t4 type="navigation" id="480"/>
-        <!-- <div class="navbar bg-primary navbar-dark navbar-expand-sm p-0">
-            <div class="container">
-                <div class="navbar-nav ki-nav-bar-primary">
-                <li class="nav-item nav-link"><a href="#"></a>Nav bar one</li>
-                    <li class="nav-item nav-link"><a href="#">Only</a></li>
-                    <li class="nav-item nav-link currentbranch0" href="#"><a href="#">Shows</a></li>
-                    <li class="nav-item nav-link"><a href="#">On</a></li>
-                    <li class="nav-item nav-link"><a href="#">Level 2</a></li>
-                    <li class="nav-item nav-link"><a href="#">onwards</a></li>
-                </div>
-            </div>
-        </div> -->
+
         <!-- navigation object : ki-level-6-nav --><t4 type="navigation" id="481"/>
-        <!-- <div class="navbar bg-info navbar-dark navbar-expand-sm mt-0 mb-0 p-0">
-            <div class="container">
-                <div class="navbar-nav ki-nav-bar-secondary">
-                    <a class="nav-item nav-link" href="#">Nav bar two</a>
-                    <a class="nav-item nav-link" href="#">Only shows</a>
-                    <a class="nav-item nav-link active" href="#">If there</a>
-                    <a class="nav-item nav-link" href="#">is</a>
-                    <a class="nav-item nav-link" href="#">a</a>
-                    <a class="nav-item nav-link" href="#">level 4</a>
-                </div>
-            </div>
-        </div> -->
+
     </div> <!-- ki-main-navs -->
         <!-- navigation object : ki-breadcrumbs --><t4 type="navigation" id="477"/>
-        <!-- <div class="container">
-            <div class="row">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bg-transparent ml-2 my-0">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item"><a href="#">Shows</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">If there</li>
-                    </ol>
-                </nav>
-            </div>
-        </div> -->
