96910a687c52a41c5f8ca485ea2019bfae0d516d
Chris Beard <coebe500@aol.com>
2018-10-22 14:54:39 +0100

    News populated partially

---
+++ t4/content-types/ki-news-story/ki-news-story.php
@@ -0,0 +1,139 @@
+<div class="container">
+    <div class="row">
+        <!-- start 9 / 3 -->
+        <div class="col-md-9">
+            <h2 class="mb-3"><t4 type="content" name="News title" output="normal" modifiers=""  /></h2>
+            <p class="font-weight-light">
+                <small>
+                    <time class="date-label" datetime="<t4 type="content" output="normal" modifiers="nav_sections" name="Date released" date_format="yyyy-MM-dd HH:mm" />"><t4 type="content" output="normal" modifiers="nav_sections" name="Date released" date_format="dd MMMM yyyy" /></time>
+                </small>
+                </p>
+
+            <figure class="figure mb-5 mx-md-3">
+                    <img class="card-img-top" src="<t4 type="content" name="Lead image: 960px x 640px" output="file" modifiers="" />" alt="<t4 type="content" name="News title" output="normal" modifiers=""  />">
+                    <figcaption class="figure-caption"><t4 type="content" name="Image caption" output="normal" modifiers=""  /></figcaption>
+            </figure>
+
+            <span class="ki-story-body">
+
+                    <p class="ki-story-snippet">
+                        <strong>
+                        A short description used on the homepage and the first line of the story.
+                    </strong>
+                    </p>
+
+                    <p>Pommy ipsum I bid you good day golly gosh it's the dogs bollocks on his tod down the local codswallop, nutter what a doddle nosh grab a jumper could murder a pint have a butcher's at this narky football, sod's law and it's spitting tip-top marmite you mean it ain't me noggin'. Doctor Who marvelous Geordie conked him one on the nose make a brew mush pennyboy gosh devonshire cream tea, supper bottled it wind up chav complete mare it's nicked. Scrote on a stag do splendid have a gander Bob's your uncle, pants ridicule conked him one on the nose bossy britches ey up chuck, on the pull nosh 10 pence mix. </p>
+
+                    <p>Jolly a tenner lass I'd reet fancy a meat and two veg bag egg's old boy for sooth knee high to a grasshopper i'll be a monkey's uncle know your onions corgi, smeg head it's nicked leisurely clock round the earhole bull dog get away with ya fancied a flutter. Ever so lovely pork scratchings cheesed off skive ever so lovely one would like Essex girls posh nosh, a bit wonky barmy narky and we all like figgy pudding atrocious scones biscuits, The Hounds of Baskerville up North who brought loaf on a stag do pants picalilly. Scrubber I'm off to Bedfordshire pot noodle baffled pompous, lass Essex girls. </p>
+                    
+                    <p>Blummin' Bad Wolf what a load of cobblers two weeks on't trot chuffed it's the dogs bollocks, a fiver crisps snotty nosed brat ee bah gum. Old girl numpty Moriarty driving a mini dignified houlligan, ear hole up North lass tad collywobbles put me soaps back on, scrubber quid pulled a right corker on his tod. Time Lord flog wind up ended up brown bread round our gaff flip flops bogroll, see a man about a dog off t'shop got a lot of brass put me soaps back on up the duff a reet bobbydazzler, cotton on chips bow ties are cool naff man and his whippet. Shakespeare shepherd's pie scarper jolly it's the bees knees shortbread yorkshire mixture, skive blighty atrocious golly The Hounds of Baskerville off the hook, working class atrocious you 'avin a laugh absobloodylootely numbskull. </p>
+                    
+                    <p>Half-inch it nigh off t'shop bottled it round our gaff fork out a bit wonky oopsy-daisies a reet bobbydazzler, bottled it pot noodle pulled a right corker bowler hat pennyboy ee bah gum. Chin up Moriarty bits 'n bobs and thus damn narky is she 'avin a laugh any road, best be off, scally bag egg's old boy could be a bit of a git Doctor Who scally Kate and Will. Houlligan golly completely crackers it's just not cricket grab a jumper trouble and strife cheesed off, eton mess anorak easy peasy it's just not cricket absolute. Round our gaff her Majesty's pleasure Union Jack bossy britches black pudding pikey fake tan it's me peepers bag egg's old boy bit of a div, bossy britches have a butcher's at this Union Jack on't goggle box pie-eyed completely starkers cheerio blimey have a gander, whizz drizzle chap a total jessie baffled old chap doing my nut in bargain Betty. </p>
+                    
+                    <p>Bit of a div rubbish at the boozer the lakes lost her marbles is she 'avin a laugh, scrumpy on the beat tad bit of alright gob odds and sods, a cuppa two weeks on't trot posh nosh it's spitting. Well fit on the pull curry sauce red telephone box don't get your knickers in a twist sorted it, middle class completely starkers clotted cream up the duff. Naff pompous bobby don't get your knickers in a twist bossy britches bottled it, spam fritters bag egg's old boy chav Bad Wolf. Whizz on a stag do tad Bad Wolf bag egg's old boy we'll be 'avin less of that, sorted it ey up duck stupendous black pudding. </p>
+                    
+                    <p>Doofer how's your father a bottle of plonk complete mare penny-dreadful, naff eton mess treacle scrote, stiff upper lip got his end away knackered. Fake tan lass golly gosh upper class dignified cottage pie what a load of guff chav it's nicked a diamond geezer what a mug cup of tea, scrote pot noodle horses for courses trouble and strife a cuppa golly gosh crisps pulled out the eating irons plum pudding chav. Stupendous picalilly loo bloody shambles it's just not cricket jolly hockey sticks, gob double dutch eton mess flabbergasted 'tis, red telephone box loo bugger smeg head. </p>
+                    
+                    <p>Lost her marbles easy peasy could murder a pint have a bash knee high to a grasshopper i'll be a monkey's uncle roast beef bargain Betty, Weeping Angels tosser scally sweets. Tip-top nicked fried toast ee bah gum bloke absobloodylootely jellied eels, Essex girls cotton on a cuppa they can sod off. Grub's up pot noodle Big Ben teacakes pot noodle punter chav for sooth Northeners real ale, pikey Southeners getting on my wick had a barney with the inlaws lost the plot daft cow scarper it's cracking flags, we'll be 'avin less of that beefeater porky-pies could murder a pint a diamond geezer laughing gear bowler hat challenge you to a duel. </p>
+                    
+                    <p>A tenner Queen Elizabeth knee high to a grasshopper i'll be a monkey's uncle see a man about a dog, down the local jellied eels. Bow ties are cool on the beat bobby scrumpy made a pig's ear of it chuffed, nosh fork out what a load of cobblers. Fancied a flutter Sherlock bowler hat how's your father rivetting stuff doolally bangers and mash lad, bent as a nine bob note trouble and strife fancy a cuppa posh nosh scrumpy is she 'avin a laugh. A reet bobbydazzler fork out scrumpy treacle rumpy pumpy oopsy-daisies, see a man about a dog what a doddle fancied a flutter. Doofer nose rag bull dog come hither it's nicked on the beat, rambunctious ridicule doing my nut in lad, dignified scarper Weeping Angels black cab. </p>
+                    
+                    <p>I could reet fancy a pot noodle for sooth accordingly, Moriarty. Got a lot of brass flabbergasted see a man about a dog nuthouse posh nosh owt chin up Time Lord some mothers do 'ave 'em, gallivanting around well chuffed golly middle class I'd reet fancy a I bid you good day queer as a clockwork orange. Loo get away with ya owt done up like a kipper picalilly a bit wonky, guinness Weeping Angels and we all like figgy pudding. </p>
+            </span>
+
+        </div> <!-- .end col-md-9 -->
+
+        <div class="col-md-3">
+                <blockquote class="blockquote bg-primary text-white rounded p-4">
+                    <p class="mb-0">So I'll give you the customary two minutes to say your prayers.</p>
+                    <!-- <footer class="blockquote-footer text-light">Someone famous in <cite title="Source Title">Source Title</cite></footer> -->
+                    <footer class="blockquote-footer text-light"><cite title="Source Title">Wile E. Coyote.</cite></footer>
+                </blockquote>
+
+                <hr />
+
+                <div class="ki-utility-links accordion mb-3" id="accordionUtilityLinks">
+                        <div class="card">
+                            <div class="card-header p-0 text-left" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        Popular links
+                                    </button>
+                                </h5>
+                            </div>
+
+                            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionUtilityLinks">
+                                <!-- collapse show to open on page load -->
+                                <div class="card-body pb-1">
+                                    <ul class="list-unstyled">
+                                        <!-- (li>a>{list link $})*25 -->
+                                        <li><a href="">list link 1</a></li>
+                                        <li><a href="">list link 2</a></li>
+                                        <li><a href="">list link 3</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header p-0 text-left" id="headingTwo">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                        data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Systems and portals
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionUtilityLinks">
+                                <div class="card-body pb-1">
+                                    <ul class="list-unstyled">
+                                        <li><a href="http://mail.keele.ac.uk">Email</a></li>
+                                        <li><a href="https://maillists.keele.ac.uk">Mailing lists</a></li>
+                                        <li><a href="https://servicedesk.keele.ac.uk">Service Desk</a></li>
+                                        <li><a href="https://publications.keele.ac.uk">Symplectic</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-3">
+                        <button type="button" class="btn btn-primary btn-block mb-2 text-left">Central services</button>
+                        <button type="button" class="btn btn-primary btn-block mb-2 text-left">A-Z</button>
+                        <button type="button" class="btn btn-primary btn-block mb-2 text-left">Policies and guidelines</button>
+                    </div>
+
+                    <div class="card text-white bg-info mb-3">
+                        <!-- <div class="card-header">Filler header optional</div> -->
+                        <img class="card-img-top" src="https://picsum.photos/960/640?image=767" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Info filler title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-info mb-3">
+                        <!-- <div class="card-header">Filler header</div> -->
+                        <img class="card-img-top" src="https://picsum.photos/960/640?image=363" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Info card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-info mb-3">
+                        <div class="card-header">Header and title on this one</div>
+                        <img class="card-img-top" src="https://picsum.photos/960/640?image=242" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Here is the title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+
+        </div> <!-- end col-md-3 -->
+        <!-- end 9 / 3 -->
+
+    </div> <!-- .row -->
+</div> <!-- .container -->
