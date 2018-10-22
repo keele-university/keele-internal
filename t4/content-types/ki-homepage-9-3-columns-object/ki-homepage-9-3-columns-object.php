4374c104a2250cbd833b22aa909b6f2c0d63a52b
Chris Beard <coebe500@aol.com>
2018-10-22 10:47:56 +0100

    Based 9-3 columns with no nav objects yet.

---
+++ t4/content-types/ki-homepage-9-3-columns-object/ki-homepage-9-3-columns-object.php
@@ -0,0 +1,205 @@
+        <!-- 9 / 3 -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+
+                    <div class="card text-white bg-primary mb-3">
+                        <img class="card-img-top" src="https://picsum.photos/960/220?image=776" alt="Card image cap">
+                        <!-- <div class="card-header">Optional header</div> -->
+                        <div class="card-body">
+                            <h5 class="card-title">Optional card title</h5>
+                            <p class="card-text">The image above can have any height but width must be 960px and
+                                recommended height is 220px</p>
+                        </div>
+                    </div>
+
+                    <!-- <a href="#">
+                        <img src="https://picsum.photos/800/400" class="img-fluid rounded" alt="Responsive image">
+                    </a> -->
+
+                    <div class="row my-5">
+                        <div class="col-sm-6">
+                            <h2 class="mb-3">Latest updates</h2>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">Keele University remembers: World War One
+                                            Centenary events</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>09 October 2018</small></p>
+                                </li>
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">Keele announces Chief Information Officer
+                                            appointment</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>11 October 2018</small></p>
+                                </li>
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">School of Psychology opens state-of-the-art human
+                                            brain electrophysiology facility</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>09 September 2018</small></p>
+                                </li>
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">Keele shortlisted for ‘University of the Year’</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>25 August 2018</small></p>
+                                </li>
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">Keele supports UKRI scheme for Future Leaders</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>12 June 2018</small></p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6">
+                            <h2 class="mb-3">Staff events</h2>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">Erasmus workshop</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>09 October 2018</small></p>
+                                </li>
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">Equality and Diversity group workshop</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>11 October 2018</small></p>
+                                </li>
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">Open days</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>09 September 2018</small></p>
+                                </li>
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">Vice Chancellor's address</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>25 August 2018</small></p>
+                                </li>
+                                <li>
+                                    <h5 class="mt-0 mb-1"><a href="#">Staff gathering at Keele hall to celebrate
+                                            nomination in awards</h5>
+                                    </a>
+                                    <p class="font-weight-light"><small>12 June 2018</small></p>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div> <!-- .row  -->
+
+                    <div class="row">
+                        <div class="col-12 mb-5">
+                            <div class="media">
+                                <div class="media-body">
+                                    <h2 class="mt-0 mb-1">Latest UEC blog</h2>
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+                                    sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                                    Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in
+                                    faucibus.
+                                </div>
+                                <img class="ki-uec__img ml-3 rounded" src="https://picsum.photos/960/640?image=502" alt="Generic placeholder image">
+                            </div>
+                        </div>
+                    </div> <!-- .row UEC -->
+
+
+                    <div class="jumbotron">
+                        <h1 class="display-4">Forums</h1>
+                        <p class="lead">Direct links to the Keele discussion boards.</p>
+                        <div class="row mt-4">
+                            <div class="col-lg-6 mb-3"><a href="" class="btn btn-primary">General chat and
+                                    announcements</a></div>
+                            <div class="col-lg-6 mb-3"><a href="" class="btn btn-primary">Job vacancies</a></div>
+                            <div class="col-lg-6 mb-3"><a href="" class="btn btn-primary">For sale or wanted</a></div>
+                            <div class="col-lg-6 mb-3"><a href="" class="btn btn-primary">Accommodation</a></div>
+                        </div>
+                        <!-- <hr class="my-4"> -->
+                        <!-- <p>Keele University discussion boards</p> -->
+                        <!-- <a class="btn btn-primary btn-lg" href="https://forums.keele.ac.uk" role="button">Forums homepage</a> -->
+                    </div>
+
+
+                </div> <!-- end col-md-9 of 9 / 3 -->
+
+                <div class="col-md-3 mb-3">
+                    <div class="ki-utility-links accordion mb-3" id="accordionUtilityLinks">
+                        <div class="card">
+                            <div class="card-header p-0 text-left" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        Popular links
+                                    </button>
+                                </h5>
+                            </div>
+
+                            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionUtilityLinks">
+                                <!-- collapse show to open on page load -->
+                                <div class="card-body pb-1">
+                                    <ul class="list-unstyled">
+                                        <!-- (li>a>{list link $})*25 -->
+                                        <li><a href="">list link 1</a></li>
+                                        <li><a href="">list link 2</a></li>
+                                        <li><a href="">list link 3</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header p-0 text-left" id="headingTwo">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                        data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Systems and portals
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionUtilityLinks">
+                                <div class="card-body pb-1">
+                                    <ul class="list-unstyled">
+                                        <li><a href="http://mail.keele.ac.uk">Email</a></li>
+                                        <li><a href="https://maillists.keele.ac.uk">Mailing lists</a></li>
+                                        <li><a href="https://servicedesk.keele.ac.uk">Service Desk</a></li>
+                                        <li><a href="https://publications.keele.ac.uk">Symplectic</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-3">
+                        <button type="button" class="btn btn-primary btn-block mb-2 text-left">Central services</button>
+                        <button type="button" class="btn btn-primary btn-block mb-2 text-left">A-Z</button>
+                        <button type="button" class="btn btn-primary btn-block mb-2 text-left">Policies and guidelines</button>
+                    </div>
+
+                    <div class="card text-white bg-info mb-3">
+                        <!-- <div class="card-header">Filler header optional</div> -->
+                        <img class="card-img-top" src="https://picsum.photos/960/640?image=767" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Info filler title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-info mb-3">
+                        <!-- <div class="card-header">Filler header</div> -->
+                        <img class="card-img-top" src="https://picsum.photos/960/640?image=363" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Info card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-info mb-3">
+                        <div class="card-header">Header and title on this one</div>
+                        <img class="card-img-top" src="https://picsum.photos/960/640?image=242" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Here is the title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div> <!-- end col-md-3 in 9 / 3 -->
+
+            </div> <!-- end 9 / 3 row -->
+        </div> <!-- end 9 / 3 container -->
