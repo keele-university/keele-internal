409c81a44580497150031082516b381327f74122
Chris Beard <coebe500@aol.com>
2018-10-23 21:05:48 +0100

    col3 root added to repo. Note that I've now added a Fawesome arrow to the dropdown buttons and have also set them to d-flex to align muddle and ml-auto to push right.

---
+++ t4/content-types/ki-col3-ROOT/ki-col3-ROOT.php
@@ -0,0 +1,72 @@
+<div class="ki-utility-links accordion mb-3" id="accordionUtilityLinks">
+                        <div class="card">
+                            <div class="card-header p-0 text-left" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link btn-block d-flex align-items-center" type="button" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        Popular links <i class="fas fa-angle-down ml-auto"></i>
+                                    </button>
+                                </h5>
+                            </div>
+
+                            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionUtilityLinks">
+                                <!-- collapse show to open on page load -->
+                                <div class="card-body pb-1">
+                                    <!-- navigation object : ki-home-9-3-popular-links --><t4 type="navigation" id="484"/>
+                                    <!-- <ul class="list-unstyled">
+                                        <!- - (li>a>{list link $})*25 - ->
+                                        <li><a href="">list link 1</a></li>
+                                        <li><a href="">list link 2</a></li>
+                                        <li><a href="">list link 3</a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header p-0 text-left" id="headingTwo">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed btn-block d-flex align-items-center" type="button" data-toggle="collapse"
+                                        data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Systems and portals <i class="fas fa-angle-down ml-auto"></i>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionUtilityLinks">
+                                <div class="card-body pb-1">
+                                    <!-- navigation object : ki-home-9-3-systems-and-portals --><t4 type="navigation" id="485"/>
+                                    <!-- <ul class="list-unstyled">
+                                        <li><a href="http://mail.keele.ac.uk">Email</a></li>
+                                        <li><a href="https://maillists.keele.ac.uk">Mailing lists</a></li>
+                                        <li><a href="https://servicedesk.keele.ac.uk">Service Desk</a></li>
+                                        <li><a href="https://publications.keele.ac.uk">Symplectic</a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="ki-utility-static-links mb-3">
+                        <!-- navigation object : ki-home-9-3-static-links BUTTONS --><t4 type="navigation" id="486"/>
+                    </div>
+
+                    <div class="card text-white bg-info mb-3">
+                        <!-- <div class="card-header">Filler header optional</div> -->
+                        <img class="card-img-top" src="https://picsum.photos/960/640?image=767" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Info filler title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-info mb-3">
+                        <!-- <div class="card-header">Filler header</div> -->
+                        <img class="card-img-top" src="https://picsum.photos/960/640?image=363" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Info card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the
+                                bulk of the card's content.</p>
+                        </div>
+                    </div>
+
+                    </div>
