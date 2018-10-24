3b047e5a78c89922176d44b6c5bdc4c1a3dc9d39
Chris Beard <bearduk@users.noreply.github.com>
2018-10-24 14:02:42 +0100

    Fix for when menus get too long in main nav.

--- resources/assets/js/utils/ki-section-header.js
+++ resources/assets/js/utils/ki-section-header.js
@@ -5,11 +5,11 @@ function init() {
     var kiSectionHeader = document.querySelector('.ki-section-header');
 
     if (kiSectionHeader) {
-        var kiSectionHeaderSectionLink = document.querySelector('h2 a');
+        var kiSectionHeaderSectionLink = kiSectionHeader.querySelector('h2 a');
         // kiSectionHeaderSectionLink.text // gives the text in the link if it exists
 
         // if there Level 2 text exists then remove d-none from the markup. Else remove the whole header block from the DOM.
-        if (kiSectionHeaderSectionLink.text !== "") {
+        if (kiSectionHeaderSectionLink && kiSectionHeaderSectionLink.text !== "") {
             kiSectionHeader.classList.remove('d-none');
         } else {
             console.log('removing kiSectionHeader: ');
