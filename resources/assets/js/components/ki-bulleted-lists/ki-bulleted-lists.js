e3cc0095f1bcd6c1fd0d35dc1c5abaed182ea545
Chris Beard <coebe500@aol.com>
2018-10-24 20:00:55 +0100

    Remove SVG due to line break issue on certain widths. Have compacted height of list items in developments instead for now as per gov.uk style

--- resources/assets/js/components/ki-bulleted-lists/ki-bulleted-lists.js
+++ resources/assets/js/components/ki-bulleted-lists/ki-bulleted-lists.js
@@ -1,6 +1,8 @@
 const vh = require('../../utils/vanilla-helper');
-const svg = '<svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"/></svg>';
-// const svg = '<i class="fas fa-angle-right"></i>';
+// const svg = '<svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow d-none d-md-inline-block"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"/></svg>';
+// const svg = '<i class="fas fa-angle-right ml-2 align-middle d-none d-md-inline-block"></i>';
+
+// Note that SVG goes to new line on certain page widths so better to implement it as background? 
 
 function init() {
 
@@ -29,7 +31,7 @@ function init() {
             const devLis = oneUl.querySelectorAll('li');
             if(devLis) {
                 _.forEach(devLis, (devLi) => {
-                    devLi.classList.add('mb-4');
+                    devLi.classList.add('mb-3');
 
                 })
             }
@@ -37,22 +39,25 @@ function init() {
         });
     }
 
+
+    vh.wrap('<h5>', '.js-ki-home-3-1 ul a, .js-ki-home-3-2 ul a', '</h5>')
+
     //////////////////////
     // add svg arrow to all links
-    const lisAhrefs = document.querySelectorAll('.js-ki-home-3-1 ul li a, .js-ki-home-3-2 ul li a');
+    // const lisAhrefs = document.querySelectorAll('.js-ki-home-3-1 ul li a, .js-ki-home-3-2 ul li a');
 
-    // now loop through all links
-    for (var i = 0; i < lisAhrefs.length; i++) {
-        let el = lisAhrefs[i];
-        const old_str = el.innerHTML;
+    // // now loop through all links
+    // for (var i = 0; i < lisAhrefs.length; i++) {
+    //     let el = lisAhrefs[i];
+    //     const old_str = el.innerHTML;
 
-        // add the link arrow class
-        vh.addClass(el, 'link-arrow');
-        // insert the svg arrows
-        el.innerHTML = old_str + svg;
-    }
+    //     // add the link arrow class
+    //     vh.addClass(el, 'link-arrow');
+    //     // insert the svg arrows
+    //     el.innerHTML = old_str + svg;
+    // }
     
-    vh.wrap('<h5>', '.js-ki-home-3-1 ul a, .js-ki-home-3-2 ul a', '</h5>')
+
     // end homepage developments panel
 }
 
