ea198138536787da3ea2be3d41ce375c75e3815e
Chris Beard <bearduk@users.noreply.github.com>
2018-10-24 12:58:34 +0100

    t4 ki-section-header. This is hidden by default and JS detects whether it has link text. If empty, it removes the header from the DOM, if it has text then remove the d-none class to show it.

---
+++ resources/assets/js/utils/ki-section-header.js
@@ -0,0 +1,25 @@
+// const vh = require('./vanilla-helper');
+
+function init() {    
+
+    var kiSectionHeader = document.querySelector('.ki-section-header');
+
+    if (kiSectionHeader) {
+        var kiSectionHeaderSectionLink = document.querySelector('h2 a');
+        // kiSectionHeaderSectionLink.text // gives the text in the link if it exists
+
+        // if there Level 2 text exists then remove d-none from the markup. Else remove the whole header block from the DOM.
+        if (kiSectionHeaderSectionLink.text !== "") {
+            kiSectionHeader.classList.remove('d-none');
+        } else {
+            console.log('removing kiSectionHeader: ');
+            console.log(kiSectionHeader);
+            kiSectionHeader.remove();
+        }
+    }
+
+}
+
+module.exports = {
+    init: init,
+}
