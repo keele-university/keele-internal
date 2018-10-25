1bbc488718a573e77ee171a8ad4abbd84edf24cc
Chris Beard <coebe500@aol.com>
2018-10-25 11:51:51 +0100

    ki- Tabs complete. Using selective output for titles only. Empty but hidden tab content blocks simply sit as empty divs in the background. Need to clean them up with JS or use selective-ouput on them.

--- t4/content-types/ki-tabs/ki-tabs.php
+++ t4/content-types/ki-tabs/ki-tabs.php
@@ -1,44 +1,59 @@
 <div class="container">
     <div class="row">
-        <div class="card mb-3">
-            <div class="card-body">
-                <ul class="nav nav-pills mb-3" id="unique<t4 type="meta" meta="content_id" />" role="tablist">
+        <div class="col-12">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <ul class="nav nav-pills mb-3 rounded p-2 border border-light" id="unique<t4 type="meta" meta="content_id" />" role="tablist">
 
-                    <li class="nav-item">
-                        <a class="nav-link active" id="tab-1-unique<t4 type="meta" meta="content_id" />" data-toggle="pill" href="#tabs-one-unique<t4 type="meta" meta="content_id" />" role="tab" aria-controls="tabs-one-unique<t4 type="meta" meta="content_id" />" aria-selected="true">Tab title text 1</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" id="tab-1-unique<t4 type="meta" meta="content_id" />" data-toggle="pill" href="#tabs-one-unique<t4 type="meta" meta="content_id" />" role="tab" aria-controls="tabs-one-unique<t4 type="meta" meta="content_id" />" aria-selected="true">
+                            <t4 type="content" name="Tab title 1" output="normal" />
+                            </a>
+                        </li>
 
-                    <!-- <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Tab title 2" format="" /> -->
-                    <li class="nav-item">
-                        <a class="nav-link" id="tab-2-unique<t4 type="meta" meta="content_id" />" data-toggle="pill" href="#tabs-two-unique<t4 type="meta" meta="content_id" />" role="tab" aria-controls="pills-profile2" aria-selected="false">Tab title text 2</a>
-                    </li>
+                        <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Tab title 2" format="<li class=&quot;nav-item&quot;>
+                            <a class=&quot;nav-link&quot; id=&quot;tab-2-unique<t4 type=&quot;meta&quot; meta=&quot;content_id&quot; />&quot; data-toggle=&quot;pill&quot; href=&quot;#tabs-two-unique<t4 type=&quot;meta&quot; meta=&quot;content_id&quot; />&quot; role=&quot;tab&quot; aria-controls=&quot;tabs-two-unique<t4 type=&quot;meta&quot; meta=&quot;content_id&quot; />&quot; aria-selected=&quot;false&quot;>
+                            $value
+                            </a>
+                        </li>" />
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" id="tab-2-unique<t4 type="meta" meta="content_id" />" data-toggle="pill" href="#tabs-two-unique<t4 type="meta" meta="content_id" />" role="tab" aria-controls="tabs-two-unique<t4 type="meta" meta="content_id" />" aria-selected="false">
+                            <t4 type="content" name="Tab title 2" output="normal" />
+                            </a>
+                        </li> -->
 
-                    <li class="nav-item">
-                        <a class="nav-link" id="tab-3-unique<t4 type="meta" meta="content_id" />" data-toggle="pill" href="#tabs-three-unique<t4 type="meta" meta="content_id" />" role="tab" aria-controls="tabs-three-unique<t4 type="meta" meta="content_id" />" aria-selected="false">Tab title text 3</a>
-                    </li>
 
-                </ul> <!-- end titles -->
-                <div class="tab-content" id="unique<t4 type="meta" meta="content_id" />Content">
+                        <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Tab title 3" format="<li class=&quot;nav-item&quot;>
+                            <a class=&quot;nav-link&quot; id=&quot;tab-3-unique<t4 type=&quot;meta&quot; meta=&quot;content_id&quot; />&quot; data-toggle=&quot;pill&quot; href=&quot;#tabs-three-unique<t4 type=&quot;meta&quot; meta=&quot;content_id&quot; />&quot; role=&quot;tab&quot; aria-controls=&quot;tabs-three-unique<t4 type=&quot;meta&quot; meta=&quot;content_id&quot; />&quot; aria-selected=&quot;false&quot;>
+                            $value
+                            </a>
+                        </li>" />
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" id="tab-3-unique<t4 type="meta" meta="content_id" />" data-toggle="pill" href="#tabs-three-unique<t4 type="meta" meta="content_id" />" role="tab" aria-controls="tabs-three-unique<t4 type="meta" meta="content_id" />" aria-selected="false">
+                            $value
+                            </a>
+                        </li> -->
+
+                    </ul> <!-- end titles -->
+                    <div class="tab-content" id="unique<t4 type="meta" meta="content_id" />Content">
 
 
-                    <div class="tab-pane fade show active" id="tabs-one-unique<t4 type="meta" meta="content_id" />" role="tabpanel" aria-labelledby="tab-1-unique<t4 type="meta" meta="content_id" />">
-                        Tabset 2 tab 1. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, suscipit aliquam soluta at tempora consectetur, nemo quas incidunt illo dolores cumque vel asperiores doloribus consequatur sed voluptas, provident possimus totam!Ad dolore minima voluptates temporibus magni ab id voluptatibus vero soluta provident ullam accusamus dolorum sequi, officiis, odit fugiat maiores velit eveniet at tempore. Laudantium temporibus assumenda exercitationem veniam alias?
-                    </div>
-                    
-                    <!-- <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Tab title 2" format="" /> -->
-                    <div class="tab-pane fade" id="tabs-two-unique<t4 type="meta" meta="content_id" />" role="tabpanel" aria-labelledby="tab-2-unique<t4 type="meta" meta="content_id" />">
-                        Tabset 2 tab 2. Dicta similique adipisci nihil repellat quia voluptate quam, necessitatibus cum laborum accusantium quisquam, nobis aliquam facere, voluptas itaque? Molestiae sed, dolor itaque quia dignissimos culpa non ad odio officia nisi.
-                        Dolore repellendus hic autem ab sapiente natus corrupti officia, excepturi voluptates adipisci nulla deserunt blanditiis, culpa praesentium incidunt qui! Mollitia, libero! Impedit, fuga expedita. Eius velit odit facilis nostrum dolore?
-                        Ipsum amet provident maxime officiis odio, at beatae quibusdam unde officia hic! At totam quisquam cupiditate distinctio similique ducimus, accusamus earum quo fuga debitis recusandae unde aliquam cum est modi.
-                        Provident eligendi, itaque a iure labore cum. Deserunt error iure maxime vero perferendis suscipit, voluptatum vitae porro magni rem eos voluptatem impedit ipsum ratione inventore earum eveniet recusandae. Deleniti, architecto.
-                    </div>
+                        <div class="tab-pane fade show active" id="tabs-one-unique<t4 type="meta" meta="content_id" />" role="tabpanel" aria-labelledby="tab-1-unique<t4 type="meta" meta="content_id" />">
+                            <t4 type="content" name="Tab content 1" output="normal" modifiers="medialibrary, nav_sections"  />
+                        </div>
+                        
+                        <!-- <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Tab title 2" format="" /> -->
+                        <div class="tab-pane fade" id="tabs-two-unique<t4 type="meta" meta="content_id" />" role="tabpanel" aria-labelledby="tab-2-unique<t4 type="meta" meta="content_id" />">
+                            <t4 type="content" name="Tab content 2" output="normal" modifiers="medialibrary, nav_sections"  />
+                        </div>
 
-                    <div class="tab-pane fade" id="tabs-three-unique<t4 type="meta" meta="content_id" />" role="tabpanel" aria-labelledby="tab-3-unique<t4 type="meta" meta="content_id" />">
-                        Tabset 2 tab 3. Provident eligendi, itaque a iure labore cum. Deserunt error iure maxime vero perferendis suscipit, voluptatum vitae porro magni rem eos voluptatem impedit ipsum ratione inventore earum eveniet recusandae. Deleniti, architecto.
-                    </div>
+                        <div class="tab-pane fade" id="tabs-three-unique<t4 type="meta" meta="content_id" />" role="tabpanel" aria-labelledby="tab-3-unique<t4 type="meta" meta="content_id" />">
+                            <t4 type="content" name="Tab content 3" output="normal" modifiers="medialibrary, nav_sections"  />
+                        </div>
 
-                </div> <!-- .tab-content -->
-            </div> <!-- .card-body -->
-        </div> <!-- .card -->
+                    </div> <!-- .tab-content -->
+                </div> <!-- .card-body -->
+            </div> <!-- .card -->
+        </div>
     </div><!-- .row -->
 </div><!-- .container -->
