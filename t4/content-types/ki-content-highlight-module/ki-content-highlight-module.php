e2057b87c302cf1af4276b94dcfb38e865c6249a
Chris Beard <coebe500@aol.com>
2018-10-25 16:28:17 +0100

    Init and completed ki- Content highlight module

---
+++ t4/content-types/ki-content-highlight-module/ki-content-highlight-module.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12 mb-3">
+            <div class="text-center my-4">
+                <hr>
+                <h3><t4 type="content" name="Title" output="normal" modifiers=""  /></h3>
+                <p><t4 type="content" name="Main text" output="normal" modifiers="medialibrary, nav_sections"  /></p>
+                <hr>
+            </div>
+        </div>
+    </div>
+</div>
