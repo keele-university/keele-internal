6b792f3ae651aac6c4a42ce67e3292ed31e6f686
Chris Beard <bearduk@users.noreply.github.com>
2018-10-26 12:13:48 +0100

    Tweak to hr on highlight module

--- t4/content-types/ki-content-highlight-module/ki-content-highlight-module.php
+++ t4/content-types/ki-content-highlight-module/ki-content-highlight-module.php
@@ -2,10 +2,10 @@
     <div class="row">
         <div class="col-12 mb-3">
             <div class="text-center my-4">
-                <hr class="mb-5">
+                <hr class="mb-5 col-6">
                 <h3><t4 type="content" name="Title" output="normal" modifiers=""  /></h3>
                 <p><t4 type="content" name="Main text" output="normal" modifiers="medialibrary, nav_sections"  /></p>
-                <hr class="mt-5">
+                <hr class="mt-5 col-6">
             </div>
         </div>
     </div>
