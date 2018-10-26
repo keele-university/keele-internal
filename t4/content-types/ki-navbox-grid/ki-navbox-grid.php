f908caf14e70dcf82d68eabc20a4a85e2a2dab1b
Chris Beard <bearduk@users.noreply.github.com>
2018-10-26 16:12:32 +0100

    Added all navbox items

--- t4/content-types/ki-navbox-grid/ki-navbox-grid.php
+++ t4/content-types/ki-navbox-grid/ki-navbox-grid.php
@@ -19,8 +19,6 @@
                 </a>
             </div> <!-- .card -->
         </div> <!-- .col -->
-
-
         <!-- <div class="col-sm-6 col-md-4 mb-3">
             <div class="card">
                 <a href="#">
@@ -35,6 +33,121 @@
                 </a>
             </div> <!- - .card - ->
         </div> <!- - .col - -> -->
+
+        <div class="col-sm-6 col-md-4 mb-3 d-none <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 2: title" format="d-block" />">
+            <div class="card">
+                <a href="<t4 type="content" name="Nav 2: section link" output="linkurl" modifiers=""  /><t4 type="content" name="Nav 2: URL link" output="normal" modifiers=""  />">
+                    
+                    <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 2: image alt text" format="<img class=&quot;card-img-top&quot; src=&quot;<t4 type=&quot;content&quot; name=&quot;Nav 2: image&quot; output=&quot;file&quot; modifiers=&quot;&quot; />&quot; alt=&quot;$value&quot;>" />
+                    
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h5 class="card-title"><t4 type="content" name="Nav 2: title" output="normal" /> <svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow d-none d-md-inline-block"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"/></svg></h5>
+                            <t4 type="content" output="selective-output" name="Nav 2: subtitle" format="<h6 class=&quot;card-subtitle mb-2 text-muted&quot;>$value</h6>" />
+                            <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                            <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 2: short text" format="<p class=&quot;card-text&quot;>$value</p>" />
+                            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                        </div>
+                    </div>
+                </a>
+            </div> <!-- .card -->
+        </div> <!-- .col -->
+
+
+
+
+        <div class="col-sm-6 col-md-4 mb-3 d-none <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 3: title" format="d-block" />">
+            <div class="card">
+                <a href="<t4 type="content" name="Nav 3: section link" output="linkurl" modifiers=""  /><t4 type="content" name="Nav 3: URL link" output="normal" modifiers=""  />">
+                    
+                    <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 3: image alt text" format="<img class=&quot;card-img-top&quot; src=&quot;<t4 type=&quot;content&quot; name=&quot;Nav 3: image&quot; output=&quot;file&quot; modifiers=&quot;&quot; />&quot; alt=&quot;$value&quot;>" />
+                    
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h5 class="card-title"><t4 type="content" name="Nav 3: title" output="normal" /> <svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow d-none d-md-inline-block"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"/></svg></h5>
+                            <t4 type="content" output="selective-output" name="Nav 3: subtitle" format="<h6 class=&quot;card-subtitle mb-2 text-muted&quot;>$value</h6>" />
+                            <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                            <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 3: short text" format="<p class=&quot;card-text&quot;>$value</p>" />
+                            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                        </div>
+                    </div>
+                </a>
+            </div> <!-- .card -->
+        </div> <!-- .col -->
+
+
+
+        
+
+
+        <div class="col-sm-6 col-md-4 mb-3 d-none <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 4: title" format="d-block" />">
+            <div class="card">
+                <a href="<t4 type="content" name="Nav 4: section link" output="linkurl" modifiers=""  /><t4 type="content" name="Nav 4: URL link" output="normal" modifiers=""  />">
+                    
+                    <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 4: image alt text" format="<img class=&quot;card-img-top&quot; src=&quot;<t4 type=&quot;content&quot; name=&quot;Nav 4: image&quot; output=&quot;file&quot; modifiers=&quot;&quot; />&quot; alt=&quot;$value&quot;>" />
+                    
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h5 class="card-title"><t4 type="content" name="Nav 4: title" output="normal" /> <svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow d-none d-md-inline-block"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"/></svg></h5>
+                            <t4 type="content" output="selective-output" name="Nav 4: subtitle" format="<h6 class=&quot;card-subtitle mb-2 text-muted&quot;>$value</h6>" />
+                            <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                            <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 4: short text" format="<p class=&quot;card-text&quot;>$value</p>" />
+                            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                        </div>
+                    </div>
+                </a>
+            </div> <!-- .card -->
+        </div> <!-- .col -->
+
+
+        
+
+
+        <div class="col-sm-6 col-md-4 mb-3 d-none <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 5: title" format="d-block" />">
+            <div class="card">
+                <a href="<t4 type="content" name="Nav 5: section link" output="linkurl" modifiers=""  /><t4 type="content" name="Nav 5: URL link" output="normal" modifiers=""  />">
+                    
+                    <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 5: image alt text" format="<img class=&quot;card-img-top&quot; src=&quot;<t4 type=&quot;content&quot; name=&quot;Nav 5: image&quot; output=&quot;file&quot; modifiers=&quot;&quot; />&quot; alt=&quot;$value&quot;>" />
+                    
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h5 class="card-title"><t4 type="content" name="Nav 5: title" output="normal" /> <svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow d-none d-md-inline-block"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"/></svg></h5>
+                            <t4 type="content" output="selective-output" name="Nav 5: subtitle" format="<h6 class=&quot;card-subtitle mb-2 text-muted&quot;>$value</h6>" />
+                            <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                            <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 5: short text" format="<p class=&quot;card-text&quot;>$value</p>" />
+                            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                        </div>
+                    </div>
+                </a>
+            </div> <!-- .card -->
+        </div> <!-- .col -->
+
+
+
+        <div class="col-sm-6 col-md-4 mb-3 d-none <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 6: title" format="d-block" />">
+            <div class="card">
+                <a href="<t4 type="content" name="Nav 6: section link" output="linkurl" modifiers=""  /><t4 type="content" name="Nav 6: URL link" output="normal" modifiers=""  />">
+                    
+                    <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 6: image alt text" format="<img class=&quot;card-img-top&quot; src=&quot;<t4 type=&quot;content&quot; name=&quot;Nav 6: image&quot; output=&quot;file&quot; modifiers=&quot;&quot; />&quot; alt=&quot;$value&quot;>" />
+                    
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h5 class="card-title"><t4 type="content" name="Nav 6: title" output="normal" /> <svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow d-none d-md-inline-block"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"/></svg></h5>
+                            <t4 type="content" output="selective-output" name="Nav 6: subtitle" format="<h6 class=&quot;card-subtitle mb-2 text-muted&quot;>$value</h6>" />
+                            <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                            <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 6: short text" format="<p class=&quot;card-text&quot;>$value</p>" />
+                            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                        </div>
+                    </div>
+                </a>
+            </div> <!-- .card -->
+        </div> <!-- .col -->
+
+
+
+        
+
+
 
 
     </div> <!-- .row -->
