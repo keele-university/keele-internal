dd0f8b42a0a90588b53765ccd1461533cd213d86
Chris Beard <bearduk@users.noreply.github.com>
2018-10-26 15:10:30 +0100

    ki- Navbox grid with one item.

---
+++ t4/content-types/ki-navbox-grid/ki-navbox-grid.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="row">
+    
+        <div class="col-sm-6 col-md-4 mb-3 d-none <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 1: title" format="d-block" />">
+            <div class="card">
+                <a href="<t4 type="content" name="Nav 1: section link" output="linkurl" modifiers=""  /><t4 type="content" name="Nav 1: URL link" output="normal" modifiers=""  />">
+                    
+                    <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 1: image alt text" format="<img class=&quot;card-img-top&quot; src=&quot;<t4 type=&quot;content&quot; name=&quot;Nav 1: image&quot; output=&quot;file&quot; modifiers=&quot;&quot; />&quot; alt=&quot;$value&quot;>" />
+                    
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h5 class="card-title"><t4 type="content" name="Nav 1: title" output="normal" /> <svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow d-none d-md-inline-block"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"/></svg></h5>
+                            <t4 type="content" output="selective-output" name="Nav 1: subtitle" format="<h6 class=&quot;card-subtitle mb-2 text-muted&quot;>$value</h6>" />
+                            <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                            <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Nav 1: short text" format="<p class=&quot;card-text&quot;>$value</p>" />
+                            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                        </div>
+                    </div>
+                </a>
+            </div> <!-- .card -->
+        </div> <!-- .col -->
+
+
+        <!-- <div class="col-sm-6 col-md-4 mb-3">
+            <div class="card">
+                <a href="#">
+                    <img class="card-img-top" src="https://picsum.photos/960/640?187" alt="Card image cap">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h5 class="card-title">Nav card title is always required <svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow d-none d-md-inline-block"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"/></svg></h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </a>
+            </div> <!- - .card - ->
+        </div> <!- - .col - -> -->
+
+
+    </div> <!-- .row -->
+</div> <!-- .container -->
