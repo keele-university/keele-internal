cea5fa0b5764da09ff363a702312b2bc4e6c2990
Chris Beard <bearduk@users.noreply.github.com>
2018-10-26 13:06:14 +0100

    k- Accordions completed with 10 accordion selective output and Markdown file for comments.

--- t4/content-types/ki-accordion/ki-accordion.md
+++ t4/content-types/ki-accordion/ki-accordion.md
@@ -12,3 +12,25 @@ _.forEach(test, (t) => {
 
 })
 ```
+
+Read the hash in the url and if it matches a accordion then add then close all tabs (remove show class) and then add show class only to the matching accordion.
+
+## A default card before being chopped up by t4 is as so:
+
+```html
+                    <div class="card">
+                        <div class="card-header bg-primary" id="uniq-headingOne">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link btn-block text-white d-flex align-items-center" type="button" data-toggle="collapse" data-target="#uniq-collapseOne" aria-expanded="true" aria-controls="uniq-collapseOne">
+                            Collapsible Group Item #1  <i class="fas fa-angle-down ml-auto"></i>
+                            </button>
+                        </h5>
+                        </div>
+
+                        <div id="uniq-collapseOne" class="collapse show" aria-labelledby="uniq-headingOne">
+                        <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                        </div>
+                    </div>
+```
--- t4/content-types/ki-accordion/ki-accordion.php
+++ t4/content-types/ki-accordion/ki-accordion.php
@@ -2,7 +2,26 @@
     <div class="row">
         <div class="col-12 mb-3">
             <div class="accordion" id="uniq-accordion-">
-                    <div class="card">
+
+                    <!-- Accordion one -->
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 1" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingOne&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseOne&quot; aria-expanded=&quot;true&quot; aria-controls=&quot;uniq-collapseOne&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
+                            </button>
+                        </h5>
+                        </div>
+                    
+                        <div id=&quot;uniq-collapseOne&quot; class=&quot;collapse show&quot; aria-labelledby=&quot;uniq-headingOne&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 1&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
+                        </div>
+                        </div>
+                    </div>" />
+                    
+                    <!-- Accordion one original before t4 dicing. Note that only accordion 1 has the 'show' class -->
+                    <!-- <div class="card">
                         <div class="card-header bg-primary" id="uniq-headingOne">
                         <h5 class="mb-0">
                             <button class="btn btn-link btn-block text-white d-flex align-items-center" type="button" data-toggle="collapse" data-target="#uniq-collapseOne" aria-expanded="true" aria-controls="uniq-collapseOne">
@@ -16,35 +35,164 @@
                             Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                         </div>
                         </div>
-                    </div>
-                    <div class="card">
-                        <div class="card-header bg-primary" id="uniq-headingTwo">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link btn-block text-white d-flex align-items-center collapsed" type="button" data-toggle="collapse" data-target="#uniq-collapseTwo" aria-expanded="false" aria-controls="uniq-collapseTwo">
-                            Collapsible Group Item #2  <i class="fas fa-angle-down ml-auto"></i>
+                    </div> -->
+
+
+                    <!-- Accordion two -->
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 2" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingTwo&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center collapsed&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseTwo&quot; aria-expanded=&quot;false&quot; aria-controls=&quot;uniq-collapseTwo&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
                             </button>
                         </h5>
                         </div>
-                        <div id="uniq-collapseTwo" class="collapse" aria-labelledby="uniq-headingTwo">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        <div id=&quot;uniq-collapseTwo&quot; class=&quot;collapse&quot; aria-labelledby=&quot;uniq-headingTwo&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 2&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
                         </div>
                         </div>
-                    </div>
-                    <div class="card">
-                        <div class="card-header bg-primary" id="uniq-headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link btn-block text-white d-flex align-items-center collapsed" type="button" data-toggle="collapse" data-target="#uniq-collapseThree" aria-expanded="false" aria-controls="uniq-collapseThree">
-                            Collapsible Group Item #3  <i class="fas fa-angle-down ml-auto"></i>
+                    </div>" />
+                    
+
+
+                    <!-- Accordion three -->
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 3" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingThree&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center collapsed&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseThree&quot; aria-expanded=&quot;false&quot; aria-controls=&quot;uniq-collapseThree&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
                             </button>
                         </h5>
                         </div>
-                        <div id="uniq-collapseThree" class="collapse" aria-labelledby="uniq-headingThree">
-                        <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        <div id=&quot;uniq-collapseThree&quot; class=&quot;collapse&quot; aria-labelledby=&quot;uniq-headingThree&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 3&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
                         </div>
                         </div>
-                    </div>
+                    </div>" />
+
+
+
+                    <!-- Accordion four -->
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 4" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingFour&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center collapsed&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseFour&quot; aria-expanded=&quot;false&quot; aria-controls=&quot;uniq-collapseFour&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
+                            </button>
+                        </h5>
+                        </div>
+                        <div id=&quot;uniq-collapseFour&quot; class=&quot;collapse&quot; aria-labelledby=&quot;uniq-headingFour&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 4&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
+                        </div>
+                        </div>
+                    </div>" />
+
+
+                    <!-- Accordion five -->
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 5" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingFive&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center collapsed&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseFive&quot; aria-expanded=&quot;false&quot; aria-controls=&quot;uniq-collapseFive&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
+                            </button>
+                        </h5>
+                        </div>
+                        <div id=&quot;uniq-collapseFive&quot; class=&quot;collapse&quot; aria-labelledby=&quot;uniq-headingFive&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 5&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
+                        </div>
+                        </div>
+                    </div>" />
+
+
+                    <!-- Accordion six -->
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 6" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingSix&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center collapsed&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseSix&quot; aria-expanded=&quot;false&quot; aria-controls=&quot;uniq-collapseSix&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
+                            </button>
+                        </h5>
+                        </div>
+                        <div id=&quot;uniq-collapseSix&quot; class=&quot;collapse&quot; aria-labelledby=&quot;uniq-headingSix&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 6&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
+                        </div>
+                        </div>
+                    </div>" />
+
+
+                    <!-- Accordion seven -->
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 7" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingSeven&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center collapsed&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseSeven&quot; aria-expanded=&quot;false&quot; aria-controls=&quot;uniq-collapseSeven&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
+                            </button>
+                        </h5>
+                        </div>
+                        <div id=&quot;uniq-collapseSeven&quot; class=&quot;collapse&quot; aria-labelledby=&quot;uniq-headingSeven&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 7&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
+                        </div>
+                        </div>
+                    </div>" />
+
+
+                    <!-- Accordion eight -->
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 8" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingEight&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center collapsed&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseEight&quot; aria-expanded=&quot;false&quot; aria-controls=&quot;uniq-collapseEight&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
+                            </button>
+                        </h5>
+                        </div>
+                        <div id=&quot;uniq-collapseEight&quot; class=&quot;collapse&quot; aria-labelledby=&quot;uniq-headingEight&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 8&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
+                        </div>
+                        </div>
+                    </div>" />
+
+
+                    <!-- Accordion nine -->
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 9" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingNine&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center collapsed&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseNine&quot; aria-expanded=&quot;false&quot; aria-controls=&quot;uniq-collapseNine&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
+                            </button>
+                        </h5>
+                        </div>
+                        <div id=&quot;uniq-collapseNine&quot; class=&quot;collapse&quot; aria-labelledby=&quot;uniq-headingNine&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 9&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
+                        </div>
+                        </div>
+                    </div>" />
+
+
+                    <!-- Accordion ten -->                                                                                                    
+                    <t4 type="content" output="selective-output" process-format="true" name="Accordion title 10" format="<div class=&quot;card&quot;>
+                        <div class=&quot;card-header bg-primary&quot; id=&quot;uniq-headingTen&quot;>
+                        <h5 class=&quot;mb-0&quot;>
+                            <button class=&quot;btn btn-link btn-block text-white d-flex align-items-center collapsed&quot; type=&quot;button&quot; data-toggle=&quot;collapse&quot; data-target=&quot;#uniq-collapseTen&quot; aria-expanded=&quot;false&quot; aria-controls=&quot;uniq-collapseTen&quot;>
+                            $value  <i class=&quot;fas fa-angle-down ml-auto&quot;></i>
+                            </button>
+                        </h5>
+                        </div>
+                        <div id=&quot;uniq-collapseTen&quot; class=&quot;collapse&quot; aria-labelledby=&quot;uniq-headingTen&quot;>
+                        <div class=&quot;card-body&quot;>
+                            <t4 type=&quot;content&quot; name=&quot;Accordion content 10&quot; output=&quot;normal&quot; modifiers=&quot;medialibrary, nav_sections&quot;  />
+                        </div>
+                        </div>
+                    </div>" />
+
+
                 </div> <!-- .accordion -->
         </div> <!-- .col-12 -->
     </div> <!-- .row -->
