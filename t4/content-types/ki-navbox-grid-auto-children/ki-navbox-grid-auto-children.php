d693f2124fad04c0ad0c035f6190cc3591c982ae
Chris Beard <bearduk@users.noreply.github.com>
2018-10-29 12:10:02 +0000

    Init ki- navbox auto children

---
+++ t4/content-types/ki-navbox-grid-auto-children/ki-navbox-grid-auto-children.php
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class="row ki-auto-child-sections">
+
+        <!-- navigation object : ki-child-sections --><t4 type="navigation" id="497"/>
+        
+	</div> <!-- .container -->
+</div> <!-- .row -->
