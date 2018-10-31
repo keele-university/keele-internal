d8c89731cdb64f0fa7930af0bd659a318fe31357
Chris Beard <coebe500@aol.com>
2018-10-31 18:09:02 +0000

    AZ file

---
+++ resources/assets/js/components/ki-az/ki-az.js
@@ -0,0 +1,37 @@
+
+const vh = require('../../utils/vanilla-helper');
+
+function init() {
+    // x.charAt(0)
+    console.log('testing kiAZ file is loaded')
+        function myFunction() {
+            var input, filter, ul, li, a, i;
+            input = document.getElementById("kiAzInput");
+            filter = input.value.toUpperCase();
+            ul = document.getElementById("myUL");
+            li = ul.getElementsByTagName("li");
+            for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByTagName("a")[0];
+                if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
+            }
+        }
+
+        //  MOVE THIS TO EXTERNAL SCRIPT
+        // perhaps wrap the above with a span around the content type
+        // and then pick up the input and add an event listener e.g.
+//         <input id="input">
+//          <script>
+//              var input = document.getElementById('input');
+//              input.addEventListener('keyup',function(){alert(input.value);});
+//          <\/script>
+
+    
+}
+
+module.exports = {
+    init: init,
+};
