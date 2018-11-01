c31d9ab723146e5bf28f5babd885b92309fdc557
Chris Beard <bearduk@users.noreply.github.com>
2018-11-01 10:27:36 +0000

    t4 ki- A to Z

---
+++ t4/content-types/ki-az/ki-az.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <div class="row">
+        <div class="js-ki-az-wrapper col-12 mb-3 unique<t4 type="meta" meta="content_id" />">
+            <!-- <input type="text" id="kiAzInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name"> -->
+
+            <div class="input-group mb-3">
+                <input type="text" id="kiAzInput" data-unique="<t4 type="meta" meta="content_id" />" class="form-control" placeholder="Filter" aria-label="Filter" aria-describedby="kiAzInput">
+                <!-- <div class="input-group-append">
+                    <span class="input-group-text"><i class="fas fa-search"></i></span>
+                </div> -->
+            </div>
+
+
+        <t4 type="content" name="Bulleted list" output="normal" modifiers="medialibrary, nav_sections"  />
+        <!-- 
+            <ul id="myUL" class="list-unstyled list-group">
+                <li class="list-group-item"><a href="#">Apple</a></li>
+                <li class="list-group-item"><a href="#">Bulletin</a></li>
+            
+                <li class="list-group-item"><a href="#">Crisp</a></li>
+                <li class="list-group-item"><a href="#">Turkey</a></li>
+                
+                <li class="list-group-item"><a href="#">Umbrella</a></li>
+                <li class="list-group-item"><a href="#">Zoid</a></li>
+                <li class="list-group-item"><a href="#">Crisp at end should be re-ordered</a></li>
+            </ul>
+        -->
+        </div> <!-- .cols-x -->
+    </div> <!-- .row -->
+</div><!-- .container -->
