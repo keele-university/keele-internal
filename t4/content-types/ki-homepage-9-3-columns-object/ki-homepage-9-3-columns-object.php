15d3998524327157a18f26b922378d418f647c85
Chris Beard <bearduk@users.noreply.github.com>
2018-11-12 15:49:13 +0000

    UEC update not UEC blog on t4 9/3 content.

--- t4/content-types/ki-homepage-9-3-columns-object/ki-homepage-9-3-columns-object.php
+++ t4/content-types/ki-homepage-9-3-columns-object/ki-homepage-9-3-columns-object.php
@@ -99,7 +99,7 @@
                             <div class="media">
                                 <img class="ki-uec__img mr-3 rounded d-none d-md-block" src="https://picsum.photos/960/640?image=502" alt="Generic placeholder image">
                                 <div class="media-body">
-                                    <h2 class="mt-0 mb-1">Latest UEC blog</h2>
+                                    <h2 class="mt-0 mb-1">Update from UEC</h2>
                                     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
                                     sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
                                     Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in
