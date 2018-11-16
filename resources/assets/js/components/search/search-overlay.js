0ad5ae8a2fb30a7ab5f09ef9e3d3dd7caac72281
Chris Beard <coebe500@aol.com>
2018-11-16 17:26:54 +0000

    Added class to overlay on click and also using opacity.

--- resources/assets/js/components/search/search-overlay.js
+++ resources/assets/js/components/search/search-overlay.js
@@ -1,17 +1,23 @@
+const vh = require('../../utils/vanilla-helper');
 
 function init(){
 
     const searchButton = document.querySelector('.js-ki-search-overlay');
     const closeOverlay = document.querySelector('.closebtn');
 
+    const overLay = document.getElementById("myOverlay");
     // Open the full screen search box 
     function openSearch() {
-        document.getElementById("myOverlay").style.display = "block";
+        overLay.style.display = "block";
+        overLay.style.opacity = ".8";
+        vh.addClass(overLay, 'testerbod');
     }
 
     // Close the full screen search box 
     function closeSearch() {
-        document.getElementById("myOverlay").style.display = "none";
+        overLay.style.display = "none";
+        overLay.style.opacity = "0";
+        vh.removeClass(overLay, 'testerbod');
     }
 
     searchButton.addEventListener('click', function(e){
