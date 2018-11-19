b18ed01c361b2d0b8d7724c9b90c697e940a172a
Chris Beard <coebe500@aol.com>
2018-11-19 11:51:23 +0000

    File Darwin not exist

--- resources/assets/js/components/search/search-main.js
+++ resources/assets/js/components/search/search-main.js
@@ -24,7 +24,8 @@ function init() {
         instantsearch.widgets.hits({
             container: '#hits',
             templates: {
-                empty: 'No results',
+                // empty: 'No results',
+                empty: 'File darwin not exist',
                 item: `
             <a href="https://www.keele.ac.uk{{urlPath}}">
                 <div class="card">
