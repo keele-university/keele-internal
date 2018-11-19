d7b96941fde9b4e09e9c41269dc52ad04492dcc1
Chris Beard <coebe500@aol.com>
2018-11-19 14:45:26 +0000

    Auto grid cleaned up

--- t4/content-types/ki-navbox-grid-auto-children/ki-navbox-grid-auto-children.php
+++ t4/content-types/ki-navbox-grid-auto-children/ki-navbox-grid-auto-children.php
@@ -5,19 +5,3 @@
         
 	</div> <!-- .container -->
 </div> <!-- .row -->
-
-
-
-// TARGET
-
-<div class="col-sm-6 col-md-4 mb-3 d-none d-block">
-    <div class="card">
-        <a href="SiteManager?ctfn=publish&amp;fnno=30&amp;chid=2&amp;lang=en&amp;sid=59003" class="h-100"><div class="card-body">
-                <div class="card-title">
-                    <h5 class="card-title">Marketing <svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow d-none d-md-inline-block"><path d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"></path></svg>
-                    </h5>
-                </div>
-            </div>
-        </a>
-    </div> <!-- .card -->
-</div> <!-- .col-x -->
