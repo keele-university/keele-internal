6f6f82f3ca9b7e60a30b5eca42a72aca9c83e6f2
Chris Beard <bearduk@users.noreply.github.com>
2018-11-22 15:43:14 +0000

    PAST and FUTURE for Events complete

---
+++ t4/content-types/ki-staff-event/ki-events-auto-PAST-FUTURE.php
@@ -0,0 +1,6 @@
+<!-- containers and jumbotrons are in nav objects before and after HTML -->
+
+<!-- navigation object : ki-core-Event-Top-Content-latest-events-pages-FUTURE --><t4 type="navigation" id="511"/>
+
+
+<!-- navigation object : ki-core-Event-Top-Content-latest-events-pages-PAST --><t4 type="navigation" id="510"/>
