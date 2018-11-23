b7519d587b1c7d0891f6a0f7ce2c6cde101a5c8a
Chris Beard <bearduk@users.noreply.github.com>
2018-11-23 14:09:39 +0000

    First drop letter now only targets p within the main story and not in the blockquotes. Tweaks on padding etc as well.

--- resources/assets/js/components/ki-news-story/ki-news-story.js
+++ resources/assets/js/components/ki-news-story/ki-news-story.js
@@ -3,7 +3,7 @@ function init(){
     var storyBlockquote = document.querySelectorAll('.fulltext-main-story blockquote');
 
     _.forEach(storyBlockquote, function(t){
-        t.classList.add('blockquote', 'bg-primary', 'text-white', 'rounded', 'p-4', 'mr-4', 'my-4');
+        t.classList.add('blockquote', 'bg-primary', 'text-white', 'rounded', 'p-4', 'mr-0', 'mr-md-5', 'my-4');
     })
 }
 
