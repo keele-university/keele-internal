1f5ff05b681fca659eb985a13460271a62a6b185
Chris Beard <bearduk@users.noreply.github.com>
2018-11-26 13:13:53 +0000

    Margin bottom to header if no breadcrumbs.

--- resources/assets/js/layout/breadcrumbs.js
+++ resources/assets/js/layout/breadcrumbs.js
@@ -11,10 +11,14 @@ function init(){
     // hide breadcrumbs if we're on the homepage.
     var homeQuery = window.location.pathname;
     var breadCrumbs = document.querySelector('.breadcrumb');
-    if ( homeQuery !== '/internal/' ) {
+    if ( homeQuery === '/internal/' ) {
+        // Add margin bottom to the header if there's no breadcrumbs beneath it (homepage)
+        var header = document.querySelector('header');
+        vh.addClass(header, 'mb-4');
+    } else {
+        // unhide the breadcrumbs
         vh.removeClass(breadCrumbs, 'd-none'); // remove d-none which is in nav obj markup
     }
-
 
 }
 
