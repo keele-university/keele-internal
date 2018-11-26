a262237a542d0dd8d3e6c1bbfe88e0e28cd5cb21
Chris Beard <bearduk@users.noreply.github.com>
2018-11-26 10:23:12 +0000

    Breadcrumb home link now in JS.

--- resources/assets/js/layout/breadcrumbs.js
+++ resources/assets/js/layout/breadcrumbs.js
@@ -1,6 +1,6 @@
 function init(){
-    var breadHome = document.querySelector('.breadcrumb-item:first-of-type a');
-    breadHome.innerHTML = '<i class="fas fa-home"></i>';
+    var breadHome = document.querySelector('.breadcrumb-item:first-of-type');
+    breadHome.innerHTML = '<a href="/internal"><i class="fas fa-home"></i></a>';
 }
 
 module.exports = {
