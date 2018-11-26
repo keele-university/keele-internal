2d9a737f4843ee8f81cc594075189f8f3a6a505b
Chris Beard <bearduk@users.noreply.github.com>
2018-11-26 10:45:48 +0000

    Hide breadcrumbs on homepage.

--- resources/assets/js/layout/breadcrumbs.js
+++ resources/assets/js/layout/breadcrumbs.js
@@ -1,3 +1,5 @@
+var vh = require('./../utils/vanilla-helper');
+
 function init(){
     var breadHome = document.querySelector('.breadcrumb-item:first-of-type');
 
@@ -5,6 +7,13 @@ function init(){
     if (breadHome !== null ){
         breadHome.innerHTML = '<a href="/internal"><i class="fas fa-home"></i></a>';
     }
+
+    // hide breadcrumbs if we're on the homepage.
+    var homeQuery = window.location.pathname;
+    if ( homeQuery === '/internal' ) {
+        vh.addClass(breadHome, 'd-none');
+    }
+
 }
 
 module.exports = {
