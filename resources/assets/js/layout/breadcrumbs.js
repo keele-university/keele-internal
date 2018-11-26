1905ae84b7ba1142807324e8a0c48ccce1f68f0e
Chris Beard <bearduk@users.noreply.github.com>
2018-11-26 10:40:53 +0000

    Breadcrumbs home link safety check.

--- resources/assets/js/layout/breadcrumbs.js
+++ resources/assets/js/layout/breadcrumbs.js
@@ -1,6 +1,10 @@
 function init(){
     var breadHome = document.querySelector('.breadcrumb-item:first-of-type');
-    breadHome.innerHTML = '<a href="/internal"><i class="fas fa-home"></i></a>';
+
+    // safety check in case breadcrumbs are ever changed in t4
+    if (breadHome !== null ){
+        breadHome.innerHTML = '<a href="/internal"><i class="fas fa-home"></i></a>';
+    }
 }
 
 module.exports = {
