299eb7bf20f08e8b0ecc9d115a1ea7186cfa3d07
Chris Beard <bearduk@users.noreply.github.com>
2018-11-29 13:22:16 +0000

    IE Fix for scrollY now pageYOffset

--- resources/assets/js/utils/ki-scroll-to-top-button.js
+++ resources/assets/js/utils/ki-scroll-to-top-button.js
@@ -9,12 +9,13 @@ function init() {
 
     window.addEventListener('scroll', _.throttle(updatePosition, 400));
 
+    // ScrollY breaks on IE11 so changing to pageYOffset
     function updatePosition(){
         var height = window.innerHeight;
-        console.log('ScrollY: '+scrollY + ', Visible height: ' + height );
+        console.log('pageYOffset: '+pageYOffset + ', Visible height: ' + height );
 
         // if user scrolls past the height of the visible window
-        if ( scrollY > height ) {
+        if ( pageYOffset > height ) {
             topButton.classList.add('is-visible');
         } else { 
             topButton.classList.remove('is-visible');
