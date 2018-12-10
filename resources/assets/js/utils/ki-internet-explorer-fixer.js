ad6a0c77687cfc0d79c80b5181e34c775463b5ff
Chris Beard <bearduk@users.noreply.github.com>
2018-12-10 09:55:27 +0000

    IE11 overlay input fix

--- resources/assets/js/utils/ki-internet-explorer-fixer.js
+++ resources/assets/js/utils/ki-internet-explorer-fixer.js
@@ -7,9 +7,15 @@ function init (){
 
     if ( isIE11 === true ) {
 
+        // cards and media fix
         document.styleSheets[0].addRule('.card','display: inline-block !important;');
         document.styleSheets[0].addRule('.card','min-width: 100% !important;');
         document.styleSheets[0].addRule('.media','display: inline-block !important;');
+        
+        // overlay input box fix (without the following it is pinned to the bottom)
+        document.styleSheets[0].addRule('#ki-js-main-search-modal-form','top: 40%;');
+        document.styleSheets[0].addRule('#ki-js-main-search-modal-form','position: fixed;');
+        document.styleSheets[0].addRule('#ki-js-main-search-modal-form','left: 35%;');
 
     }
 
