760d47533d9af3310ff336bbfad2626ac9337141
Chris Beard <bearduk@users.noreply.github.com>
2018-12-13 10:07:03 +0000

    Brand and templates dropdown added.

--- t4/content-types/ki-col3-ROOT/ki-col3-ROOT.php
+++ t4/content-types/ki-col3-ROOT/ki-col3-ROOT.php
@@ -22,6 +22,7 @@
                                 </div>
                             </div>
                         </div>
+
                         <div class="card">
                             <div class="card-header p-0 text-left" id="headingTwo">
                                 <h5 class="mb-0">
@@ -34,6 +35,28 @@
                             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionUtilityLinks">
                                 <div class="card-body pb-1">
                                     <!-- navigation object : ki-home-9-3-systems-and-portals --><t4 type="navigation" id="485"/>
+                                    <!-- <ul class="list-unstyled">
+                                        <li><a href="http://mail.keele.ac.uk">Email</a></li>
+                                        <li><a href="https://maillists.keele.ac.uk">Mailing lists</a></li>
+                                        <li><a href="https://servicedesk.keele.ac.uk">Service Desk</a></li>
+                                        <li><a href="https://publications.keele.ac.uk">Symplectic</a></li>
+                                    </ul> -->
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-header p-0 text-left" id="headingThree">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed btn-block d-flex align-items-center" type="button" data-toggle="collapse"
+                                        data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        Brand and templates <i class="fas fa-angle-down ml-auto"></i>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionUtilityLinks">
+                                <div class="card-body pb-1">
+                                    <!-- navigation object : ki-home-9-3-brand-and-templates --><t4 type="navigation" id="520"/>
                                     <!-- <ul class="list-unstyled">
                                         <li><a href="http://mail.keele.ac.uk">Email</a></li>
                                         <li><a href="https://maillists.keele.ac.uk">Mailing lists</a></li>
