b951848b8e352e6fff498d12782550c11dd6de54
Chris Beard <bearduk@users.noreply.github.com>
2018-12-17 16:06:37 +0000

    External link for search

--- t4/content-types/ki-search-dom/ki-search-dom.php
+++ t4/content-types/ki-search-dom/ki-search-dom.php
@@ -1,9 +1,10 @@
-
         <div class="container">
             <div class="row">
                 <div class="col-12 py-4">
                     <div id="search-box" class="js-ki-trigger-main-searchmain">
                         <!-- SearchBox widget will appear here -->
+                    </div>
+                    <div id="search-box-external" class="d-flex justify-content-md-end"><a href="#!" class="d-flex align-items-center">search the external website<svg viewBox="0 0 16 16" width="16" height="16" class="icon-arrow"><path style="fill: #333;" d="M7.2 12.73l-1.41-1.41 3.29-3.29-3.29-3.29L7.2 3.32l4.71 4.71-4.71 4.7z"></path></svg></a>
                     </div>
                 </div>
             </div>
