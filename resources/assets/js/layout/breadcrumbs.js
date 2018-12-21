9640f7b8bec2a36ecfb6bba758f86a8248da226b
Chris Beard <bearduk@users.noreply.github.com>
2018-12-21 13:40:25 +0000

    Internal is now intranet

--- resources/assets/js/layout/breadcrumbs.js
+++ resources/assets/js/layout/breadcrumbs.js
@@ -5,13 +5,13 @@ function init(){
 
     // safety check in case breadcrumbs are ever changed in t4
     if (breadHome !== null ){
-        breadHome.innerHTML = '<a href="/internal"><i class="fas fa-home"></i></a>';
+        breadHome.innerHTML = '<a href="/intranet"><i class="fas fa-home"></i></a>';
     }
 
     // hide breadcrumbs if we're on the homepage.
     var homeQuery = window.location.pathname;
     var breadCrumbs = document.querySelector('.breadcrumb');
-    if ( homeQuery !== '/internal/' ) {
+    if ( homeQuery !== '/intranet/' ) {
         // unhide the breadcrumbs
         vh.removeClass(breadCrumbs, 'invisible'); // remove invisible which is in nav obj markup
     }
