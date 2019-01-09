37a7dbb590c4d67bb1cdcb442a73152ce7b7b586
Chris Beard <bearduk@users.noreply.github.com>
2019-01-09 15:27:22 +0000

    Tagging event on homepage list media with word Event so as not to get confused with publish dates of news items

--- t4/content-types/ki-staff-event/ki-staff-event-listmedia-formatter.php
+++ t4/content-types/ki-staff-event/ki-staff-event-listmedia-formatter.php
@@ -11,7 +11,7 @@
                                 </p>" />
                                 <p class="font-weight-light">
                                     <small>
-                                        <time class="date-label" datetime="<t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="yyyy-MM-dd HH:mm" />"><t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="dd MMMM yyyy" /></time> <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Optional end date" format=" - <t4 type=&quot;content&quot; output=&quot;normal&quot; modifiers=&quot;nav_sections&quot; name=&quot;Optional end date&quot; date_format=&quot;dd MMMM yyyy&quot; />" />
+                                        Event: <time class="date-label" datetime="<t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="yyyy-MM-dd HH:mm" />"><t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="dd MMMM yyyy" /></time> <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Optional end date" format=" - <t4 type=&quot;content&quot; output=&quot;normal&quot; modifiers=&quot;nav_sections&quot; name=&quot;Optional end date&quot; date_format=&quot;dd MMMM yyyy&quot; />" />
                                     </small>
                                 </p>
                             </div>
