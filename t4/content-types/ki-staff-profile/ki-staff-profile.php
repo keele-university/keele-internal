e5d0c19f09c09cfaa2c6cb6ad14f0f532a73252d
Chris Beard <bearduk@users.noreply.github.com>
2019-02-06 10:36:39 +0000

    Staff profile base complete. Needs t4 tags.

---
+++ t4/content-types/ki-staff-profile/ki-staff-profile.php
@@ -0,0 +1,48 @@
+<div class="container">
+    <div class="row mb-5">
+        <div class="col-md-10 offset-md-1">
+            <div class="layout-staff card text-center">                        
+                <img class="card-img-top d-none d-md-block" src="/k-core/intra/images/ki-staff-background-c.jpg" alt="Card image cap">
+                <div class="avatar mb-0 mt-3">
+                    <img alt="" src="https://www.keele.ac.uk/mac-staff/staff/Cheryl-200x200.jpg">
+                </div>
+                <div class="card-body">
+
+                    <div class="col-md-10 offset-md-1">
+                        <span class="d-block mb-4">
+                            <h3 class="card-title mb-0">Cheryl Belford</h3>
+                            <p class="font-italic">Senior Graphic Designer</p>
+                        </span>
+
+                        <p class="card-text mb-4">Cheryl leads our creative and cost-effective in-house design service, which is available across print and digital channels. Experience and knowledge in all aspects of developing and improving brand guidelines, concepts, design, print and production. The in-house design team work alongside the marketing team producing a wide range of marketing collateral across the wider University on brand.</p>
+
+                        <div class="row">
+                            <div class="col"><hr></div>
+                            <div class="col-auto">
+                                <h4>Team</h4>
+                            </div>
+                            <div class="col"><hr></div>
+                        </div>
+                        <p>Marketing</p>
+
+                        <div class="row">
+                            <div class="col"><hr></div>
+                            <div class="col-auto">
+                                <h4>Contact details</h4>
+                            </div>
+                            <div class="col"><hr></div>
+                        </div>
+                        <p class="font-weight-light mb-0">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                            <a href="mailto:c.belford@keele.ac.uk">c.belford@keele.ac.uk</a>
+                        </p>
+                        <p class="font-weight-light"><i class="fa fa-phone" aria-hidden="true"></i> 
+                            <span class="mctel"><a href="tel:01782 734749">01782 734749</a></span>
+                        </p>
+                        <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+                    </div><!-- card-body -->
+
+            </div> <!-- layout-staff -->
+        </div> <!-- col -->
+    </div> <!-- row -->
+</div><!-- container -->
