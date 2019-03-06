187be89c3b5f066b8a13fe26d142381898d89512
Chris Beard <bearduk@users.noreply.github.com>
2019-03-06 16:50:00 +0000

    Staff profile az working

--- t4/content-types/ki-staff-profile/ki-staff-profile-az.php
+++ t4/content-types/ki-staff-profile/ki-staff-profile-az.php
@@ -1,1 +1,5 @@
-<li><!-- navigation object : ki- Staff profiles az return to index --><t4 type="navigation" id="534"/></li>
+<li><!-- navigation object : ki- Staff profiles az return to index -->
+  
+  <t4 type="content" name="Last name" output="normal" modifiers=""  />, <t4 type="content" name="First name" output="normal" modifiers=""  />  <t4 type="navigation" id="534"/>
+
+</li>
