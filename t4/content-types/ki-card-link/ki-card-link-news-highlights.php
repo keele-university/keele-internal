60209a5737ed3e36943747a8aefb5c37adf2bb1e
Chris Beard <coebe500@aol.com>
2019-07-04 14:08:19 +0100

    ki-card-link-news-highlights

---
+++ t4/content-types/ki-card-link/ki-card-link-news-highlights.php
@@ -0,0 +1,8 @@
+                    <div class="ki-home-news-rhs card text-white bg-info mb-3">
+                        
+                        <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Image alt text" format="<img class=&quot;card-img-top&quot; src=&quot;<t4 type=&quot;content&quot; name=&quot;Image 960x640&quot; output=&quot;file&quot; modifiers=&quot;&quot; />&quot; alt=&quot;$value&quot;>" />
+                        <div class="card-body">
+                            <t4 type="content" output="selective-output" modifiers="medialibrary, nav_sections" name="Card title" format="<h5 class=&quot;card-title&quot;>$value</h5>" />
+                            <t4 type="content" output="selective-output" modifiers="medialibrary, nav_sections" name="Bulleted list" format="$value" />
+                        </div>
+                    </div>
