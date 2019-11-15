8e75ce7e9f79cf6e86dcec8265ff38311263b921
Chris Beard <coebe500@aol.com>
2019-11-15 15:31:40 +0000

    Events listmedia now shows date, time and location

--- t4/content-types/ki-staff-event/ki-staff-event-listmedia-formatter.php
+++ t4/content-types/ki-staff-event/ki-staff-event-listmedia-formatter.php
@@ -1,4 +1,3 @@
-                    <!-- set up for the homepage pinned stories -->
                     <a href="<t4 type="content" output="fulltext" modifiers="nav_sections" use-element="true" filename-element="hyphenated-page-slug-name" name="Event title" />">
                         <li class="media">
                             <img class="mr-3 mb-4 lazyload blur-up" src="data:image/gif;base64,R0lGODlhAQABAIAAAPT09AAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="<t4 type="content" name="Lead image: 960px x 300px" output="file" modifiers="" />" alt="<t4 type="content" name="Image caption" output="normal" modifiers=""  />">
@@ -11,7 +10,10 @@
                                 </p>" />
                                 <p class="font-weight-light">
                                     <small>
-                                        Event: <time class="date-label" datetime="<t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="yyyy-MM-dd HH:mm" />"><t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="dd MMMM yyyy" /></time> <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Optional end date" format=" - <t4 type=&quot;content&quot; output=&quot;normal&quot; modifiers=&quot;nav_sections&quot; name=&quot;Optional end date&quot; date_format=&quot;dd MMMM yyyy&quot; />" />
+                                    <i class="fas fa-calendar-day"></i> <time class="date-label" datetime="<t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="yyyy-MM-dd HH:mm" />"><t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="dd MMMM yyyy" /></time> <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Optional end date" format=" - <t4 type=&quot;content&quot; output=&quot;normal&quot; modifiers=&quot;nav_sections&quot; name=&quot;Optional end date&quot; date_format=&quot;dd MMMM yyyy&quot; />" /> <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Event time" format=", $value" />
+
+                                    <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Location" format="<br />Location: $value" />
+                                    <!-- <i class="fas fa-calendar-day"></i> <time class="date-label" datetime="<t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="yyyy-MM-dd HH:mm" />"><t4 type="content" output="normal" modifiers="nav_sections" name="Start date" date_format="dd MMMM yyyy" /></time> <t4 type="content" output="selective-output" process-format="true" modifiers="medialibrary, nav_sections" name="Optional end date" format=" - <t4 type=&quot;content&quot; output=&quot;normal&quot; modifiers=&quot;nav_sections&quot; name=&quot;Optional end date&quot; date_format=&quot;dd MMMM yyyy&quot; />" /> -->
                                     </small>
                                 </p>
                             </div>
