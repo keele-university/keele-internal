33c2004b9f8cf76d538402d9dc76e7b53be539b6
Chris Beard <coebe500@aol.com>
2021-02-22 12:09:48 +0000

    ki- embed iframe init

---
+++ t4/content-types/ki-embed-iframe/ki-embed-iframe.php
@@ -0,0 +1,4 @@
+<!-- 16:9 aspect ratio -->
+<div class="embed-responsive <t4 type="content" name="Aspect ratio" output="normal" modifiers=""  />">
+    <t4 type="content" name="iframe code" output="normal" modifiers=""  />
+</div>
